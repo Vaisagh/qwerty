2e97b8e98f1cae875fe916f4296355d338e2a6df
VDK <vaisaghasiet@gmail.comgit config --global user.name>
2020-02-19 10:39:53 +0530

    ffsf

--- src/App.js
+++ src/App.js
@@ -1,95 +1,94 @@
 import React from 'react';
 import logo, { ReactComponent } from './logo.svg';
-import productTile from './components/producttile';
 import './App.css';
-const productsarray = [
-  {
-    "id": 1,
-    "title" : "HP Core i3 6th Gen",
-    "price" : "Rs 45000",
-    "image" : "https://rukminim1.flixcart.com/image/704/704/jdyuefk0/computer/t/u/4/hp-na-laptop-original-imaf2rdcgrw8nzfv.jpeg?q=70"
-  },
-  
-  {
-    "id": 2,
-    "title" : "MSI GV 62 7RE",
-    "price" : "Rs 70000",
-    "image" : "https://rukminim1.flixcart.com/image/704/704/ja73ki80/computer/v/v/x/msi-na-gaming-laptop-original-imaeztu7nz9e6mz9.jpeg?q=70"
-  },
-
-  {
-    "id" : 3,
-    "title" : "Airgun",
-    "price" : "Rs 6000",
-    "image" : "https://www.airsoftgunindia.com/wp-content/uploads/2018/01/img_6810.jpg" 
-  }
-]
-
-
+import productfile from './components/producttile';
 class App extends React.Component {
-
-  constructor(props) {
+  constructor(props){
     super(props)
     this.state = {
       firstState: 'hello World',
-      cartCount: 0,
-      alertOnCartCount: 10,
-      limitreached: false,
-      products : []
+      cartCount: 0 ,
+      alertOncartCount : 10 ,
+      products : [],
+      cart : []
     }
   }
   componentDidMount() {
+    this.loadproducts();
+    this.initCart();
     this.setState({
       firstState: 'Welcome to our shop'
     })
   }
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state)
-    if (this.state.cartCount >= this.state.alertOnCartCount) {
-      this.state.limitreached = true
-      alert("Oops,Count exceeded more than 10 items")
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.cartCount >= this.state.alertOncartCount)
+    {
+      alert('Oops cart limit exceeded')
     }
   }
-  loadProducts()
+  initCart()
   {
+    let mycart = localStorage.getItem('cart')
+    mycart = JSON.parse(mycart)
     this.setState({
-      
-        products : productsarray
-    }
-    )
+      cart: mycart || []
+    })
   }
-  
-  addCount() {
-    if (this.state.limitreached == false) {
-      const newCount = this.state.cartCount + 1;
-      this.setState({
-        cartCount: newCount
-      })
-    }
+  addtoCart(product)
+  {
+    const newCart = this.state.cart;
+    newCart.push(product);
 
+    localStorage.setItem('cart', JSON.stringify(newCart))
+    this.setState({
+      cart: newCart
+    })
   }
-  
+  loadproducts()
+  {
+    fetch('http://my-json-server.typicode.com/shiyasvp92/sample_products/products', {
+      method: 'GET'
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((data)=> {
+      console.log(data)
+      this.setState({
+        products: data
+      })
+    })
+    .catch((error)=> {
+      console.error(error)
+    })
+  }
+  viewCart()
+  {
+    console.log(this.state.cart)
+  }
   render() {
-    const productlist = this.state.products.map((product)=>{
-      return productTile(this.addCount.bind(this),product)
+    const productlist = this.state.products.map((product) =>{
+      return productfile(this.addtoCart.bind(this),product)
     })
     console.log(productlist)
     return (
       <div>
         <nav className="navbar  navbar-dark bg-primary">
           <a className="navbar-brand" href="#">
-            <img src="hprttps://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Famazon%2Famazon_PNG22.png&f=1&nofb=1"
+            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Famazon%2Famazon_PNG22.png&f=1&nofb=1"
               width="30" height="30" className="d-inline-block align-top" alt="" />
             Amazon
           </a>
           {this.state.firstState}
-          <button onClick={() =>{this.loadProducts()}}>load products</button>
-          <button type="button" className="btn btn-success"
-          >Cart {this.state.cartCount}
+          <button onClick={() =>{this.loadproducts()}}>load products</button>
+          <button type="button" className="btn btn-success" onclick={() => {
+            this.viewCart();
+          }}>
+            Cart ({this.state.cart.length})
           </button>
         </nav>
         <div className="container">
-          <p className="sh">Laptops</p>
+          <p className="sh">Laptops</p>   
           {productlist}
           <div className="row d-flex justify-content-end">
             <button type="button" className="btn btn-primary" id="er">CHECKOUT </button>
@@ -99,4 +98,5 @@ class App extends React.Component {
     )
   }
 }
+
 export default App;
